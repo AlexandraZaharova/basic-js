1fc7f765b1b965358c9868f5ccff8acf116bfc75
AlexandraZaharova <zakharova911@gmail.com>
2022-05-02 13:12:15 +0300

    feat: add class VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(typeMachine) {
+    if (typeof typeMachine == 'undefined') {
+      this.typeMachine = true;
+    } else {
+      this.typeMachine = typeMachine;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (typeof message == 'undefined' || typeof key == 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let m = message.toUpperCase().split('');
+    let k = key.toUpperCase();
+    let result = [];
+    let a = 0;
+    let b = 0;
+    let sum = 0;
+    for (let i=0; i<m.length; i++) {
+      if (str.includes(m[i])) {
+        a = str.indexOf(m[i]);
+        if (i>=k.length) {
+          b = str.indexOf(k[i%k.length]);
+        } else b = str.indexOf(k[i]);
+        sum = a+b;
+        if (sum >= str.length) {
+          sum = sum - str.length;
+        }
+        result.push(str[sum]);
+      } else {
+        result.push(m[i]);
+        m.splice(i, 1);
+        i--;
+      }
+    }
+    if (this.typeMachine) {
+      return result.join('');
+    } else {
+      return result.reverse().join('');
+    }
+  }
+  decrypt(message, key) {
+    if (typeof message == 'undefined' || typeof key == 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let m = message.toUpperCase().split('');
+    let k = key.toUpperCase();
+    let result = [];
+    let a = 0;
+    let b = 0;
+    let sum = 0;
+    for (let i=0; i<m.length; i++) {
+      if (str.includes(m[i])) {
+        a = str.indexOf(m[i]);
+        if (i>=k.length) {
+          b = str.indexOf(k[i%k.length]);
+        } else b = str.indexOf(k[i]);
+        sum = a-b;
+        if (sum >= str.length) {
+          sum = sum - str.length;
+        }
+        if (sum<0) {
+          result.push(str[str.length+sum])
+        } else result.push(str[sum]);
+      } else {
+        result.push(m[i]);
+        m.splice(i, 1);
+        i--;
+      }
+    }
+    if (this.typeMachine) {
+      return result.join('');
+    } else {
+      return result.reverse().join('');
+    }
   }
 }
 

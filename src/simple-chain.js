b390c1c75e7d1159d59cae51da2efec442711f22
AlexandraZaharova <zakharova911@gmail.com>
2022-05-02 23:57:26 +0300

    feat: add chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (arguments.length == 0) value = '';
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    position = position - 1;
+    if (position >= 0 && position < this.chain.length) {
+      this.chain.splice(position, 1);
+      return this;
+    }
+    this.chain = [];
+    throw new Error("You can't remove incorrect link!");
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let string = '';
+    let pattern = ' )~~( ';
+    if (this.chain.length == 1) {
+      string = '( ' + this.chain[0] + ' )';
+    } else {
+      for (let i = 0; i < this.chain.length; i++) {
+        if (i == 0) {
+          string += '( ' + this.chain[i];
+        } else if (i == this.chain.length - 1) {
+          string += pattern + this.chain[i] + ' )';
+        } else {
+          string += pattern + this.chain[i];
+        }
+      }
+    }
+    this.chain = [];
+    return string;
   }
 };
 
